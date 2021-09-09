4d0a43797da961dca565ef239ec16dd76babf552
luisriego <luisriego@hotmail.com>
2021-09-09 10:24:53 -0300

    tested GetUserById and getUserByEmail, Ok

--- tests/Functional/Controller/User/GetUserByEmailControllerTest.php
+++ tests/Functional/Controller/User/GetUserByEmailControllerTest.php
@@ -2,46 +2,35 @@
 
 namespace App\Functional\Controller\User;
 
+use App\Tests\Functional\FunctionalTestBase;
 use Hautelook\AliceBundle\PhpUnit\RecreateDatabaseTrait;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
-class GetUserByEmailControllerTest extends WebTestCase
+class GetUserByEmailControllerTest extends FunctionalTestBase
 {
     use RecreateDatabaseTrait;
 
-    private const ENDPOINT = '/api/v1/users/user';
-
-    private static ?KernelBrowser $client = null;
-
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        if (null === self::$client) {
-            self::$client = static::createClient();
-            self::$client->setServerParameter('CONTENT_TYPE', 'application/json');
-        }
-    }
+    private const ENDPOINT = '/api/v1/users/user-by-email';
 
     public function testGetUserByEmail(): void
     {
-        self::$client->request(Request::METHOD_GET, \sprintf('%s/peter@api.com', self::ENDPOINT));
+        self::$baseClient->request(Request::METHOD_GET, \sprintf('%s/peter@api.com', self::ENDPOINT));
 
-        $response = self::$client->getResponse();
+        $response = self::$baseClient->getResponse();
 
         self::assertEquals(JsonResponse::HTTP_OK, $response->getStatusCode());
         $responseData = \json_decode($response->getContent(), true);
-        // self::assertEquals('Peter', $responseData['user']['name']);
+        self::assertEquals('Peter', $responseData['user']['name']);
     }
 
     public function testGetUserForNonExistingEmail(): void
     {
-        self::$client->request(Request::METHOD_GET, \sprintf('%s/brian@api.com', self::ENDPOINT));
+        self::$baseClient->request(Request::METHOD_GET, \sprintf('%s/brian@api.com', self::ENDPOINT));
 
-        $response = self::$client->getResponse();
+        $response = self::$baseClient->getResponse();
 
         self::assertEquals(JsonResponse::HTTP_NOT_FOUND, $response->getStatusCode());
     }
--- tests/Functional/Controller/User/GetUserByIdControllerTest.php
+++ tests/Functional/Controller/User/GetUserByIdControllerTest.php
@@ -2,47 +2,43 @@
 
 namespace App\Functional\Controller\User;
 
+use App\Tests\Functional\FunctionalTestBase;
 use Hautelook\AliceBundle\PhpUnit\RecreateDatabaseTrait;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
-class GetUserByIdControllerTest extends WebTestCase
+class GetUserByIdControllerTest extends FunctionalTestBase
 {
-//    use RecreateDatabaseTrait;
-//
-//    private const ENDPOINT = '/api/v1/users/user';
-//
-//    private static ?KernelBrowser $client = null;
-//
-//    public function setUp(): void
-//    {
-//        parent::setUp();
-//
-//        if (null === self::$client) {
-//            self::$client = static::createClient();
-//            self::$client->setServerParameter('CONTENT_TYPE', 'application/json');
-//        }
-//    }
-//
-//    public function testGetUserById(): void
-//    {
-//        self::$client->request(Request::METHOD_GET, \sprintf('%s/da1ffa07-51bd-4fb7-aec2-b88385edd5ff', self::ENDPOINT));
-//
-//        $response = self::$client->getResponse();
-//
-//        self::assertEquals(JsonResponse::HTTP_OK, $response->getStatusCode());
-//        $responseData = \json_decode($response->getContent(), true);
-//        self::assertEquals('Peter', $responseData['user']['name']);
-//    }
+    use RecreateDatabaseTrait;
 
-//    public function testGetUserForNonExistingId(): void
-//    {
-//        self::$client->request(Request::METHOD_GET, \sprintf('%s/brian@api.com', self::ENDPOINT));
-//
-//        $response = self::$client->getResponse();
-//
-//        self::assertEquals(JsonResponse::HTTP_NOT_FOUND, $response->getStatusCode());
-//    }
+    private const ENDPOINT = '/api/v1/users/user';
+
+    public function testGetUserById(): void
+    {
+        // self::$baseClient->request(Request::METHOD_GET, '/api/v1/users/user-by-email/peter@api.com');
+        // $response = self::$baseClient->getResponse();
+        // $responseData = \json_decode($response->getContent(), true);
+        // $id = $responseData['user']['id'];
+
+        $id = $this->getPeterId();
+
+        self::$baseClient->request(Request::METHOD_GET, \sprintf('%s/%s', self::ENDPOINT, $id));
+
+        $response = self::$baseClient->getResponse();
+
+        self::assertEquals(JsonResponse::HTTP_OK, $response->getStatusCode());
+        $responseData = \json_decode($response->getContent(), true);
+        self::assertEquals('Peter', $responseData['user']['name']);
+    }
+
+   public function testGetUserForNonExistingId(): void
+   {
+       self::$baseClient->request(Request::METHOD_GET, \sprintf('%s/123456', self::ENDPOINT));
+
+       $response = self::$baseClient->getResponse();
+
+       self::assertEquals(JsonResponse::HTTP_NOT_FOUND, $response->getStatusCode());
+   }
 }
